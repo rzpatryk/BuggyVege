3fcbcab67d642f169c70b2e14bd8f90de2c1ee1d
rzpatryk <rzeszotarski.pat@gmail.com>
2025-07-29 16:22:54 +0200

    feat: Add password management routes for forgot, reset, and change password functionality

--- Backend/RoutesMySql/authRouterMysql.js
+++ Backend/RoutesMySql/authRouterMysql.js
@@ -8,5 +8,7 @@ router.route('/login').post(authController.login);
 // Chroniony endpoint do sprawdzenia profilu
 router.route('/profile').get(authController.protect, authController.getProfile);
 router.route('/profile/test/:userId').get(authController.getProfileTest);
-
+router.route('/forgot-password').post(authController.forgotPassword);
+router.route('/reset-password').post(authController.resetPassword);
+router.route('/change-password').post(authController.protect, authController.changePassword);
 module.exports = router;
